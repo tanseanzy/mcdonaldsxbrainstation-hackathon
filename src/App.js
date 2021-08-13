792616a4abf9da106528bd8bf5bb4ce120ea6747
Mohamed Aweis <Mohamed.aweis.mohamed@gmail.com>
2021-08-13 13:25:09 -0400

    added offer pages

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Footer from "./components/Footer";
 
 // pages import
 import HomePage from "./pages/HomePage";
+import OfferPage from "./pages/OfferPage/OfferPage";
+import PersonalOfferPage from "./pages/PersonalizedOfferPage/POPage";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <main className="main">
           <Switch>
             <Route path="/" exact component={HomePage} />
+            <Route path="/Offers" exact component={OfferPage} />
+            <Route path="/PersonalOffers" exact component={PersonalOfferPage} />
           </Switch>
         </main>
         {/* <Footer /> */}
